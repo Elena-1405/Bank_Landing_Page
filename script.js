649e0ead1cf26b230b2b65a998cfb4dfb982d61e
Elena Petrova <helenofspb@gmail.com>
2025-07-10 15:26:01 +0300

    Tabbed components implemented

--- script.js
+++ script.js
@@ -11,62 +11,85 @@ const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
-  e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+	e.preventDefault();
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+	if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal();
+	}
 });
 
 const header = document.querySelector('.header');
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.innerHTML =
-  'We use cookies for improving functionality and analytics. <button class="btn btn--close-cookie">Got it</button> '; //to read and set content
+	'We use cookies for improving functionality and analytics. <button class="btn btn--close-cookie">Got it</button> '; //to read and set content
 header.append(message);
 document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', () => message.remove());
+	.querySelector('.btn--close-cookie')
+	.addEventListener('click', () => message.remove());
 
 //Scroll
 
-btnScrollTo.addEventListener('click', e => {
-  const s1coords = section1.getBoundingClientRect();
-  //console.log(e.target.getBoundingClientRect()); //choose left and top in the obj
+btnScrollTo.addEventListener('click', (e) => {
+	const s1coords = section1.getBoundingClientRect();
+	//console.log(e.target.getBoundingClientRect()); //choose left and top in the obj
 
-  // OLD SCHOOL, excludes scroll bars
-  // window.scrollTo(
-  //   s1coords.left + window.pageXOffset,
-  //   s1coords.top + window.pageYOffset
-  // );
-  // window.scrollTo({
-  //   left: s1coords.left + window.pageXOffset,
-  //   top: s1coords.top + window.pageYOffset,
-  //   behavior: 'smooth',
-  // });
+	// OLD SCHOOL, excludes scroll bars
+	// window.scrollTo(
+	//   s1coords.left + window.pageXOffset,
+	//   s1coords.top + window.pageYOffset
+	// );
+	// window.scrollTo({
+	//   left: s1coords.left + window.pageXOffset,
+	//   top: s1coords.top + window.pageYOffset,
+	//   behavior: 'smooth',
+	// });
 
-  section1.scrollIntoView({ behavior: 'smooth' }); // Modern way
+	section1.scrollIntoView({ behavior: 'smooth' }); // Modern way
 });
 
 //Smooth Scrolling. Event delegation
-document.querySelector('.nav__links').addEventListener('click', e => {
-  e.preventDefault();
-  if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href');
-    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
-  }
+document.querySelector('.nav__links').addEventListener('click', (e) => {
+	e.preventDefault();
+	if (e.target.classList.contains('nav__link')) {
+		const id = e.target.getAttribute('href');
+		document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+	}
+});
+
+//Tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', (e) => {
+	const clicked = e.target.closest('.operations__tab'); //finds the closest parent, button itself
+
+	if (!clicked) return; //Guard clause
+
+	//remove activate classes
+	tabs.forEach((t) => t.classList.remove('operations__tab--active'));
+	tabsContent.forEach((c) => c.classList.remove('operations__content--active'));
+
+	//Active tab
+	clicked.classList.add('operations__tab--active'); //taken from CSS
+
+	//Activate content area
+	document
+		.querySelector(`.operations__content--${clicked.dataset.tab}`) //taken from HTML
+		.classList.add('operations__content--active'); //taken from CSS
 });
