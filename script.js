fab48ef920b096fa7d279122103eae72fc0f6144
Elena Petrova <helenofspb@gmail.com>
2025-07-11 13:36:44 +0300

    Reveal sections function done

--- script.js
+++ script.js
@@ -128,4 +128,23 @@ window.addEventListener('scroll', () => {
 	else nav.classList.remove('sticky');
 });
 
-//
+//Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+	const [entry] = entries;
+	if (!entry.isIntersecting) return;
+
+	entry.target.classList.remove('section--hidden');
+	observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+	root: null,
+	threshold: 0.2,
+});
+
+allSections.forEach((section) => {
+	sectionObserver.observe(section);
+	section.classList.add('section--hidden');
+});
